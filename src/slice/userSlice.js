c2371898aafb65932f0b39cc510344b128d80cb8
nGhart <107068059+nGhart@users.noreply.github.com>
2023-06-13 04:07:03 +0000

    Update userSlice.js

--- src/slice/userSlice.js
+++ src/slice/userSlice.js
@@ -1,18 +1,34 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    users: [],
-}
+  users: [
+    { name: 'Bernard', email: 'bernard@gmail.com', gen: 24, id: '988o7' },
+    { name: 'Credo', email: 'credo@gmail.com', gen: 24, id: '988o71z' },
+  ],
+};
 const userSlice = createSlice({
-    name: "users",
-    initialState,
-    reducers: {
-        addNewUser: (state, action) => {
-            state.users = [...state.users, action.payload]
-        },
-        deleteUser: () => { },
-        editUser: () => { }
-    }
-})
-export const {addNewUser, deleteUser, editUser}=userSlice.actions;
+  name: 'users',
+  initialState,
+  reducers: {
+    addNewUser: (state, action) => {
+      state.users = [...state.users, action.payload];
+    },
+    deleteUser: (state, action) => {
+      state.users = state.users.filter((item) => {
+        if (item.id !== action.payload) {
+          return item;
+        }
+      });
+    },
+    editUser: (state, action) => {
+      state.users = state.users.map((item) => {
+        if (item.id === action.payload.id) {
+          return action.payload.updatedUser;
+        }
+        return item;
+      });
+    },
+  },
+});
+export const { addNewUser, deleteUser, editUser } = userSlice.actions;
 export default userSlice.reducer;

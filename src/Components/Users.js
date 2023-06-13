98d956683cc365fbbc30f2045467f17afc12511f
nGhart <107068059+nGhart@users.noreply.github.com>
2023-06-13 04:04:36 +0000

    Update Users.js

--- src/Components/Users.js
+++ src/Components/Users.js
@@ -1,49 +1,21 @@
 import React from 'react';
-import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
+import SingleUser from './SingleUser';
 
 const Users = () => {
-    const state = useSelector((state) => {
-        return state.userReducer
-    });
-    console.log(state);
-    return (
-        <Container>
-            <Row>
-                <h1 className='users'>Users</h1>
-                <Col className='userDisplay'>
-                    {state.users.map((singleUser) => {
-                        return (
-                        <Card className='box' key={singleUser.id}>
-                            <Card.Body>
-                                <Card.Title>
-                                    Name: {singleUser.name}
-                                </Card.Title>
-                                <Card.Text>
-                                    Email: {singleUser.email}
-                                </Card.Text>
-                                <Card.Text>
-                                    Gen: {singleUser.gen}
-                                </Card.Text>
-                                <div className='userModification'>
-                                    <Button variant="primary" className='userButton' 
-                                    // onClick={handleShow}
-                                    >Edit</Button>
-                                    <Button variant="primary" className='userButton' 
-                                    // onClick={handleDelete}
-                                    >Delete</Button>
-                                </div>
-                            </Card.Body>
-                        </Card>
-                        )
-                    })}
-                </Col>
-            </Row>
-        </Container>
-    )
-}
-
-
-
-
+  const state = useSelector((state) => state.userReducer.users);
+  return (
+    <Container>
+      <Row>
+        <h1 className="users">Users</h1>
+        <Col className="userDisplay" xs={12} sm={4}>
+          {state.map((singleUser) => {
+            return <SingleUser key={singleUser.id} user={singleUser} />;
+          })}
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 export default Users;
